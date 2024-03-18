cb7e8e276ebe2d402a2bec66843ec14b894772fd
KRISHNAKUMARC27 <krishnakumarc27@gmail.com>
2024-03-18 11:58:20 +0530

    hosting node JS

---
+++ create-react-app/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const os = require('os'); // Add the os module to get network interfaces
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+const apiProxy = createProxyMiddleware('/api', {
+  target: 'http://localhost:8080', // Your Spring Boot server address
+  changeOrigin: true,
+  secure: false, // Set to false to ignore SSL certificate errors, but be cautious with this in production
+  logLevel: 'debug',
+  pathRewrite: {
+    '^/api': '' // This will remove /api from the URL path
+  }
+});
+
+app.use('/api', apiProxy);
+
+// Serve static files from the 'build' directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Function to get the local IP address
+function getLocalIpAddress() {
+  const interfaces = os.networkInterfaces();
+  for (const iface of Object.values(interfaces)) {
+    for (const alias of iface) {
+      if (alias.family === 'IPv4' && !alias.internal) {
+        return alias.address;
+      }
+    }
+  }
+  return 'localhost';
+}
+
+const ipAddress = getLocalIpAddress();
+
+http.createServer(app).listen(4001, '0.0.0.0', () => {
+  console.log(`HTTP server running at http://${ipAddress}:4001`);
+});
